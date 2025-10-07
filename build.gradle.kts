ca52494f5ec31ac07b142c9d4327f1ffc1609a55
vickram odero <vickramodero6@gmail.com>
2025-10-07 14:33:51 +0300

    Created branch main

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    `maven-publish`
 }
 
 group = "com.pesapal"
@@ -10,8 +11,8 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-core:2.3.3")
-    implementation("io.ktor:ktor-client-cio:2.3.3")
+    implementation("io.ktor:ktor-client-core:3.3.0")
+    implementation("io.ktor:ktor-client-cio:3.3.0")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.3")
     implementation("io.ktor:ktor-serialization-gson:2.3.3")
     implementation("com.google.code.gson:gson:2.10.1")
@@ -25,6 +26,48 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(8)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "com.github.vick-ram"
+            artifactId = "pesapal-kotlin-client"
+            version = version
+
+            pom {
+                name.set("Pesapal Kotlin SDK")
+                description.set("Unofficial Kotlin SDK for Pesapal Payment API")
+                url.set("https://github.com/vick-ram/pesapal-kotlin-client")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("vick-ram")
+                        name.set("Vickram Odero")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/vick-ram/pesapal-kotlin-client.git")
+                    developerConnection.set("scm:git:ssh://github.com/vick-ram/pesapal-kotlin-client.git")
+                    url.set("https://github.com/vick-ram/pesapal-kotlin-client")
+                }
+            }
+        }
+    }
 }
